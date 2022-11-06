e17637edbd8c364ab9e2dd6f7c5b401b0d304b79
hawary-id <hawaryproject@gmail.com>
2022-11-06 23:18:44 +0700

    feat : master update search component page vectors

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,15 +2,11 @@ $(document).ready(function () {
   $(window).scroll(function () {
     var scroll = $(window).scrollTop();
     if (scroll >= 50) {
-      $("#search").addClass("top-0 bg-gray-50");
+      $("#search").addClass("top-0");
       $("#search").removeClass("top-12");
-      $("#recomended").addClass("top-14");
-      $("#recomended").removeClass("top-26");
     } else if (scroll <= 50) {
-      $("#search").removeClass("top-0 bg-gray-50");
+      $("#search").removeClass("top-0");
       $("#search").addClass("top-12");
-      $("#recomended").removeClass("top-14");
-      $("#recomended").addClass("top-26");
     }
   });
   $("#btn-sort").click(function (e) {
