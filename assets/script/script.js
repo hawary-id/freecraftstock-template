7fef7ae5b2613d234aedcda1e5d5e2e26163a955
hawary-id <hawaryproject@gmail.com>
2022-11-09 20:34:39 +0700

    update menu dropdown (mobile menu)

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,28 +1,32 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll >= 50) {
-            $("#search").addClass("top-0");
-            $("#search").removeClass("top-12");
-        } else if (scroll <= 50) {
-            $("#search").removeClass("top-0");
-            $("#search").addClass("top-12");
-        }
-    });
-    $("#btn-sort").click(function(e) {
-        e.preventDefault();
-        $("#list-sort").toggleClass("hidden");
-    });
-    $(".menu-dropdown").click(function(e) {
-        e.preventDefault();
-        $(".menu-item").toggle();
-    });
-    $("#nav-menu").click(function(e) {
-        e.preventDefault();
-        $("#nav-sm").removeClass("hidden");
-    });
-    $("#nav-close").click(function(e) {
-        e.preventDefault();
-        $("#nav-sm").addClass("hidden");
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      $("#search").addClass("top-0");
+      $("#search").removeClass("top-12");
+    } else if (scroll <= 50) {
+      $("#search").removeClass("top-0");
+      $("#search").addClass("top-12");
+    }
+  });
+  $("#btn-sort").click(function (e) {
+    e.preventDefault();
+    $("#list-sort").toggleClass("hidden");
+  });
+  $(".menu-dropdown").click(function (e) {
+    e.preventDefault();
+    $(".menu-item").toggle();
+  });
+  $("#nav-menu").click(function (e) {
+    e.preventDefault();
+    $("#nav-sm").removeClass("hidden");
+  });
+  $("#nav-close").click(function (e) {
+    e.preventDefault();
+    $("#nav-sm").addClass("hidden");
+  });
+  $("#nav-dropdown").click(function (e) {
+    e.preventDefault();
+    $("#menu-dropdown").toggleClass("hidden");
+  });
 });
